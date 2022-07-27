3e8c48e21aab170872881c90330d2ceddf66ce23
Yuki Tazawa <tazawa@liquidinc.asia>
2022-07-27 10:33:52 +0900

    Fix gitignore

--- lib/typescript/src/hook.d.ts
+++
@@ -1,3 +0,0 @@
-import { Frame } from 'react-native-vision-camera';
-import { Barcode, BarcodeFormat } from '.';
-export declare function useScanBarcodes(types: BarcodeFormat[]): [(frame: Frame) => void, Barcode[]];
